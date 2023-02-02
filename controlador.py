a461779831fb3c4e16c599a25941544883d9252d
Carlos Javier Calderon Delgado <89323622+CarlosJCD@users.noreply.github.com>
2023-02-02 04:00:59 +0000

    Version con .json

--- controlador.py
+++ controlador.py
@@ -1,104 +1,102 @@
-from re import compile
-from modelo import LectorCSV, EscritorCSV
-from Excepciones.Registros import CuentaExistenteExcepcion, NumClienteExistenteExcepcion
-from Excepciones.Credenciales import CredencialesInvalidasExcepcion
+from re import compile, sub
+from excepciones import ClienteExistente, CuentaExistente, CredencialesInvalidas, CuentaConSaldo
+from modelo import Escritor, Lector
+from os import listdir, mkdir
+from os.path import exists
 
 class Controlador:
-    """ 
-    Atributos
-    ---------
-    nombre_archivo: str
-        Cadena que representa la ruta del archivo con el cual va a estar trabajando el controlador
-    
-    Metodos
-    -------
-    __validar_credenciales(registro: dict)
-            Valida las credenciales de un registro
-    crear_Registro(num_cliente: int, nombre: str, num_cuenta: int, saldo: float)
+    def __init__(self):
+        self.base_path = "Clientes/"
+        self.escritor: Escritor = Escritor()
+        self.lector: Lector = Lector()
 
+    def __validar_credenciales(self,registro: dict):
+        patronNum = compile("^\d{16}$")
+        patronSaldo = compile("^\d*.\d*$")
+        patronNombre = compile("^[A-Za-z]{2,25}\s[A-Za-z]{2,25}$")
 
-    """
-
-    def __init__(self, nombre_archivo: str) -> None:
-        self.nombre_archivo = nombre_archivo
-
-    def __validar_credenciales(self,registro: dict) -> None:
-        patron_num = compile("^\d{16}$")
-        patron_saldo = compile("^\d*.\d*$")
-        patron_nombre = compile("^[A-Za-z]{2,25}\s[A-Za-z]{2,25}$")
-
-        condicion_num_cuenta = patron_num.search(str(registro.get("numCuenta"))) or not registro.get("numCuenta")
-        condicion_num_cliente = patron_num.search(str(registro.get("numCliente")))
-        condicion_saldo = patron_saldo.search(str(registro.get("saldo")))
-        condicion_nombre = patron_nombre.search(str(registro.get("nombre")))
-        try:
-            if not condicion_num_cuenta:
-                raise CredencialesInvalidasExcepcion(credencial = "Num. Cuenta", valor = registro.get("numCuenta"))
-            elif not condicion_num_cliente:
-                raise CredencialesInvalidasExcepcion(credencial = "Num. Cliente", valor= registro.get("numCliente"))
-            elif not condicion_saldo:
-                raise CredencialesInvalidasExcepcion(credencial = "Saldo", valor= registro.get("saldo"))
-            elif not condicion_nombre:
-                raise CredencialesInvalidasExcepcion(credencial = "Nombre", valor= registro.get("nombre"))
-        except CredencialesInvalidasExcepcion as excepcion_credencial:
-            print(excepcion_credencial)           
-    def crear_registro(self,num_cliente: int, nombre: str, num_cuenta: int, saldo: float = 0.0):
-        """
-        Crea un registro valido con la siguiente estructura:
-
-        num_cliente,nombre,num_cuenta,saldo
-
-        Parametros
-        --------------
-        num_cliente: int
-            Entero que representa el numero de cliente. Debe ser de 16 digitos
-        nombre: String
-            Cadena que representa el nombre del cliente
-        num_cuenta: int
-            Entero que representa el numero de cuenta. Debe ser de 16 digitos
-        saldo: float
-            Flotante que representa el saldo de la cuenta. Su valor por defecto es 0.0
+        condicionNumCuenta = patronNum.search(str(registro.get("numCuenta")))
+        condicionNumCliente = patronNum.search(str(registro.get("numCliente")))
+        condicionSaldo = patronSaldo.search(str(registro.get("saldo")))
+        condicionNombre = patronNombre.search(str(registro.get("nombre")))
         
-        Excepciones
-        -----------
-        NumClienteExistenteExcepcion: 
-            Excepcion levantada cuando num_cliente del nuevo registro y num_cliente del registro actual coinciden pero los nombres de cliente no coinciden
-        NumCuentaExistenteExcepcion:
-            Excepcion levantada cuando el valor de num_cuenta ya fue registrado.
-        
-        Return
-        ------
-        None
-        """
-        registro_nuevo = {
+        if not condicionNumCuenta:
+            raise CredencialesInvalidas(credencial= "Num. Cuenta", valor = registro.get("numCuenta"))
+        elif not condicionNumCliente:
+            raise CredencialesInvalidas(credencial = "Num. Cliente", valor= registro.get("numCliente"))
+        elif not condicionSaldo:
+            raise CredencialesInvalidas(credencial = "Saldo", valor= registro.get("saldo"))
+        elif not condicionNombre:
+            raise CredencialesInvalidas(credencial = "Nombre", valor= registro.get("nombre"))
+            
+    def crear_cliente(self,num_cliente: int, nombre: str, num_cuenta: int = None, saldo: float = 0.0):
+        registroNuevo = {
             "numCliente": num_cliente,
             "nombre": nombre,
             "numCuenta": num_cuenta,
             "saldo": saldo
         }
-        self.__validar_credenciales(registro_nuevo)
+        self.__validar_credenciales(registroNuevo)
 
-        if not num_cuenta:
-            registro_nuevo["saldo"] = None
+        lista_clientes: list = listdir(self.base_path)
 
-        try:
-            lector = LectorCSV(self.nombre_archivo)
-            registros = lector.leer_archivo()
-            for registro in registros:
-                registroExistente={
-                    "numCliente": registro.split(",")[0],
-                    "nombre": registro.split(",")[1],
-                    "numCuenta": registro.split(",")[2],
-                    "saldo": registro.split(",")[3]
-                }
-                if (str(registro_nuevo.get("numCliente")) == registroExistente.get("numCliente")) and (registro_nuevo.get("nombre") != registroExistente.get("nombre")):
-                    raise NumClienteExistenteExcepcion(f"ERROR: Numero de Cliente {registro_nuevo.get('numCliente')} ya registrado")
-                elif str(registro_nuevo.get("numCuenta")) == registroExistente.get("numCuenta"):
-                    raise NumClienteExistenteExcepcion(f"ERROR: Numero de Cuenta #{registro_nuevo.get('numCuenta')} ya registrado")
-            
-            escritor = EscritorCSV(self.nombre_archivo)
-            escritor.add(registro_nuevo)
-        except CuentaExistenteExcepcion as excepcion:
-            print(excepcion.mensaje)
-        except NumClienteExistenteExcepcion as excepcion:
-            print(excepcion.mensaje)
+        if str(num_cliente) in lista_clientes:
+            raise ClienteExistente(f"Cliente #{num_cliente} ya registrado")
+        else:
+            if not exists(f"{self.base_path}{num_cliente}/"):
+                self.escritor.registrar_cliente({"numCliente": num_cliente,"nombre": nombre,})
+                self.escritor.registrar_cuenta({"numCuenta": num_cuenta,"saldo": saldo}, num_cliente)
+            else:
+                lista_cuentas: list = [sub(".json","",cuenta) for cuenta in listdir(f"{self.base_path}{num_cliente}/")]
+                if num_cuenta in lista_cuentas:
+                    raise CuentaExistente(f"Cuenta #{num_cuenta} ya registrada")
+                self.escritor.registrar_cuenta({"numCuenta": num_cuenta,"saldo": saldo},num_cliente)        
+
+    def crear_cuenta(self, num_cliente: int, num_cuenta: int, saldo: float = 0.0):
+        client_path = f"{self.base_path}{num_cliente}/"
+        if exists(client_path):
+            lista_cuentas = [sub(".json","",cuenta) for cuenta in listdir(client_path)]
+            if num_cuenta in lista_cuentas:
+                raise CuentaExistente(f"Cuenta #{num_cuenta} ya registrada")
+            self.escritor.registrar_cuenta({"numCuenta":num_cuenta,"saldo":saldo},num_cliente)
+
+    def actualizar_cliente(self, num_cliente: int, nombre: str):
+        patron_num = compile("^\d{16}$")
+        patron_nombre = compile("^[A-Za-z]{2,25}\s[A-Za-z]{2,25}$")
+
+        if patron_num.search(str(num_cliente)) and patron_nombre.search(nombre):
+            self.escritor.actualizar_cliente(num_cliente,nombre)
+        else:
+            if not patron_num.search(str(num_cliente)):
+                raise CredencialesInvalidas(credencial = "Num. Cliente", valor = num_cliente)
+            else:
+                raise CredencialesInvalidas(credencial = "Nombre", valor = nombre)
+
+    def actualizar_cuenta(self, num_cliente: int, num_cuenta: int, saldo: float):
+        patron_num = compile("^\d{16}$")
+        patron_saldo = compile("^\d*.\d*$")
+
+        if patron_num.search(num_cliente) and patron_num.search(num_cuenta) and patron_saldo.search(saldo):
+            self.escritor.actualizar_cuenta(num_cliente,num_cuenta,"saldo",saldo)
+        else:
+            if not patron_num.search(num_cuenta):
+                raise CredencialesInvalidas(credencial= "Num. Cuenta", valor = num_cuenta)
+            elif not patron_num.search(num_cliente):
+                raise CredencialesInvalidas(credencial = "Num. Cliente", valor= num_cliente)
+            elif not patron_saldo.search(saldo):
+                raise CredencialesInvalidas(credencial = "Saldo", valor= saldo)
+
+    def eliminar_cuenta(self,num_cliente: int, num_cuenta: int):
+        patron_num = compile("^\d{16}$")
+
+        if patron_num.search(str(num_cliente)) and patron_num.search(str(num_cuenta)):
+            saldo: float = self.lector.leer_cuenta(num_cliente,num_cuenta).get("saldo")
+            if saldo == 0.0:
+                self.escritor.eliminar_cuenta(num_cliente,num_cuenta)
+            else:
+                raise CuentaConSaldo(f"Imposible eliminar la cuenta #{num_cuenta} con saldo {saldo}")
+        else:
+            if not patron_num.search(num_cuenta):
+                raise CredencialesInvalidas(credencial= "Num. Cuenta", valor = num_cuenta)
+            else:
+                raise CredencialesInvalidas(credencial = "Num. Cliente", valor= num_cliente)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from controlador import Controlador
-from modelo import LectorCSV
-from numpy.random import randint
+from modelo import Lector
 
-if __name__=="__main__":
-    controlador = Controlador("ejemplo.csv")
-    lector = LectorCSV("ejemplo.csv")
+if __name__ == "__main__":
+    controlador: Controlador = Controlador()
+    controlador.crear_cliente(7177451234454407,"Carlos Calderon",3070898702472936,100.2)
